12bbad8c23f34b61cfec1cfdb20736b2de2d4dcb
sunnatalie <natalie.sun2001@gmail.com>
2023-03-30 15:43:39 +0300

    fixed shopping cart

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -16,6 +16,7 @@ const ShoppingCart: FC<ShoppingCartProps> = ({ isOpen }: ShoppingCartProps) => {
         <div>
             {isOpen && (
             <div>
+                <div onClick={closeCart} className={styles.ShoppingCart__overlay}></div>
                 <div className={styles.ShoppingCart}>
                     <div className={styles.ShoppingCart__cartHeader}>
                         <button onClick={closeCart} className={styles.ShoppingCart__closeButton}>x</button>
@@ -29,13 +30,13 @@ const ShoppingCart: FC<ShoppingCartProps> = ({ isOpen }: ShoppingCartProps) => {
                     </div>
                     <div className={styles.ShoppingCart__cartFooter}>
                         {cartQuantity > 0 ? (<div>
-                            Grand total: {" "}
-                            {cartItems.reduce((total,cartItem) => {
+                            <span>Grand total: {" "}</span>
+                            <span>${cartItems.reduce((total,cartItem) => {
                                 const item = storeItems.find(i => i.id === cartItem.id);
                                 // return (total + ((item?.price || 0) * cartItem.quantity));
                                 return Math.round(((total + ((item?.price || 0) * cartItem.quantity)) + Number.EPSILON) * 100) / 100
-                            }, 0)}
-                        </div>): (<p className={styles.ShoppingCart__cartMain}>Please add some items to the cart and come back!</p>)}
+                            }, 0)}</span>
+                        </div>): (<span className={styles.ShoppingCart__cartMain}>Please add some items to the cart and come back!</span>)}
                     </div>
                 </div>
             </div>)}
